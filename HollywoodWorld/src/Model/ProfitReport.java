5681fc4b5f45ebcac9f0d0c2236047cc829a3c17
Reynaldo Marrufo <Reynaldo Marrufo@DESKTOP-OT29L3B>
2016-11-30 17:20:32 -0600

    clases bases del modulo de reportes, verifica si les hizo falta algun atributo, metodo u otra cosa, y me dices

---
+++ HollywoodWorld/src/Model/ProfitReport.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Reynaldo Marrufo
+ */
+public class ProfitReport {
+    
+    public ProfitReport(
+            String employeeName,
+            Date startDate,
+            Date endDate,
+            ArrayList<String[]> registryList,
+            double totalProfit,
+            double averageProfit
+    ){
+        this.employeeName = employeeName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.registryList = registryList;
+        this. totalProfit = totalProfit;
+        this.averageProfit = averageProfit;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public ArrayList<String[]> getRegistryList() {
+        return registryList;
+    }
+
+    public void setRegistryList(ArrayList<String[]> registryList) {
+        this.registryList = registryList;
+    }
+
+    public double getTotalProfit() {
+        return totalProfit;
+    }
+
+    public void setTotalProfit(double totalProfit) {
+        this.totalProfit = totalProfit;
+    }
+    
+
+    public double getAverageProfit() {
+        return averageProfit;
+    }
+
+    public void setAverageProfit(double averageProfit) {
+        this.averageProfit = averageProfit;
+    }
+    
+    public double totalProfit(){
+        return 0;
+    }
+    
+    public double averageProfit(){
+        return 0;
+    }
+    
+    
+    
+    public Date startDate;
+    public Date endDate;
+    public String employeeName;
+    public ArrayList<String[]> registryList;
+    public double totalProfit;
+    public double averageProfit;
+    
+}
