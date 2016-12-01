c62574891eeea0e331bcebd29379620a2a759731
RobertSuaus <roberto_ivan@outlook.es>
2016-11-30 20:01:07 -0600

    Completado modulo de renta

---
+++ HollywoodWorld/src/Controller/BusinessAnalyst.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Robert
+ */
+public class BusinessAnalyst {
+    /*El Analista de negocio se encarga de llenar los reportes solicitados 
+    por los agentes de finanzas con los datos correspondientes del negocio.*/
+    
+    public static ProfitReport fillProfitReport(
+        ProfitReport requestedReport
+    ){
+        
+    }
+    
+    public static TendencyReport fillTendencyReport(
+        TendencyReport requestedReport
+    ){
+        
+    }
+    
+    public static PerformanceReport fillPerformanceReport(
+        PerformanceReport requestedReport
+    ){
+        
+    }
+}
